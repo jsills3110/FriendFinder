6cbe2f41800781fa89a6eabc46e2eb8a6b96d2ee
jsills3110 <jsills3110@gmail.com>
2019-11-23 18:57:13 -0800

    First commit.

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+var express = require("express");
+
+// Create an express server
+var app = express();
+
+// Set the initial port
+var PORT = process.env.PORT || 8080;
+
+// Set up the app to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
